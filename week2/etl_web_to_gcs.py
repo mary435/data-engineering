9e8a3667bb21b48bcaedaa4a3e7af2fda6b8cc9f
mary435 <mary.orihuela@gmail.com>
2023-02-01 13:37:43 -0300

    q1

--- week2/etl_web_to_gcs.py
+++ week2/etl_web_to_gcs.py
@@ -17,26 +17,47 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 @task(log_prints=True)
 def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
-    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
-    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    #df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+    #df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    df["lpep_pickup_datetime"] = pd.to_datetime(df["lpep_pickup_datetime"])
+    df["lpep_dropoff_datetime"] = pd.to_datetime(df["lpep_dropoff_datetime"])
+    
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
     return df
 
+@task()
+def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
+    """Write DataFrame out locally as parquet file"""
+    path = Path(f"data/{color}/{dataset_file}.parquet")
+    df.to_parquet(path, compression="gzip")
+    return path
+
+@task()
+def write_gcs(path: Path) -> None:
+    """Upload local parquet file to GCS"""
+    gcs_block = GcsBucket.load("zoom-gcs")
+    gcs_block.upload_from_path(
+        from_path=path, 
+        to_path=path)
+    return
+
 @flow()
 def etl_web_to_gcs() -> None:
-    """The main ETL function"""
-    color = "yellow"
-    year = 2021
+    """The main ETL function""" 
+    #color = "yellow"
+    color = "green"
+    #year = 2021
+    year = 2020
     month = 1
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
 
     df = fetch(dataset_url)
-    #df_clean = clean(df)
-    #path = write_local(df_clean, color, dataset_file)
-    #write_gcs(path)
+    df_clean = clean(df)
+    path = write_local(df_clean, color, dataset_file)
+    write_gcs(path)
 
 
 if __name__ == "__main__":
