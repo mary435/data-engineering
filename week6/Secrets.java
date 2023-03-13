cb8a2ca8dc76df6e7927acda3453a29ef87bcd2c
mary435 <mary.orihuela@gmail.com>
2023-03-13 16:04:58 -0300

    hw6

---
+++ week6/Secrets.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Secrets {
+    public static final String KAFKA_CLUSTER_KEY = "5RVSYJYLDJB2H227";
+    public static final String KAFKA_CLUSTER_SECRET = "ebATLFoRosnSX/Nd7P0xx1q5buc1bt5WiYK6jTHq7IHNFDhnyEptF8l+7YitVAxu";
+
+    public static final String SCHEMA_REGISTRY_KEY = "REPLACE_WITH_SCHEMA_REGISTRY_KEY";
+    public static final String SCHEMA_REGISTRY_SECRET = "REPLACE_WITH_SCHEMA_REGISTRY_SECRET";
+
+}
